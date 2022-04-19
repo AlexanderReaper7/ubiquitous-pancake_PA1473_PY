dcbaabc06637699bc82f7c3f24083e15ced2ea85
victormoberg <103108037+victormoberg@users.noreply.github.com>
2022-04-19 15:09:07 +0200

    My commit

---
+++ project/lift.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pybricks-micropython
+from robot import Robot
+
+class Command_lift:
+    def __init__(self, speed = 50, duty_limit = 60):
+        self.speed = speed
+        self.duty_limit = duty_limit
+    
+    def lift_up(self, robot):
+        if robot.touch_sensor == True:
+            robot.lift_motor.run_until_stalled(self.speed, then=Stop.HOLD, self.duty_limit)
+
