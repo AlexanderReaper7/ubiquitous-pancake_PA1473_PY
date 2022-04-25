2c91735609bc81b855ce0c1fca63d5f5397c46a9
ludvigpernetun <103107770+ludvigpernetun@users.noreply.github.com>
2022-04-25 09:32:24 +0200

    stuff

--- project/lift.py
+++ project/lift.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 from robot import Robot
 
-class Command_lift:
-    def __init__(self, speed = 50, duty_limit = 60):
+class Command_lift(Command_Base):
+    def __init__(self, name="Lift", speed = 50, duty_limit = 60):
+        super().__init__(name=name)
         self.speed = speed
         self.duty_limit = duty_limit
     
-    def lift_up(self, robot):
+    def run(self, robot):
         if robot.touch_sensor.pressed() == True:
             robot.lift_motor.run_until_stalled(self.speed, then=Stop.HOLD, self.duty_limit)
 
--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@
 import sys
 import __init__
 from commands import Command_Queue, Command_Halt, Command_Lambda
+from lift import Command_Lift
 from line_following import Command_Line_Following
 from robot import Robot
 
@@ -17,6 +18,7 @@ def main():
     # initialize command queue
     command_queue.append(Command_Line_Following(lambda: robot.touch_sensor.pressed(), "Follow standard line until the front touch sensor is pressed."))
     command_queue.append(Command_Halt())
+    command_queue.append(Command_Lift())
     command_queue.append(Command_Lambda(lambda: print("this is an example lambda command"), name="Print example text."))
     nested_command_queue = Command_Queue("Nested Command Queue")
     nested_command_queue.append(Command_Lambda(lambda: print("this is an example of a nested command queue"), name="Print example of nested command queue."))
