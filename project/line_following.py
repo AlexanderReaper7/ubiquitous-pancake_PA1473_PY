2673e45e5b4b01813c5a589e09b3f09a8be78c85
Alexander Öberg <stativetroller7@gmail.com>
2022-04-27 18:48:02 +0200

    added color grouping algo, begin work on advanced line following

--- project/line_following.py
+++ project/line_following.py
@@ -42,3 +42,39 @@ class Command_Line_Following(Command_Base):
             # Set the drive base speed and turn rate.
             robot.drivebase.drive(self.speed, turn_rate)
 
+class Command_exit_circle_at_color(Command_Base):
+    """
+    drives to a specified color
+    """
+    def __init__(self, color, name="Go to Color", speed=100, gain=1.7):
+        """
+        follow the central circle line until encontering the specified color and stop.
+        Paramaters:
+        color: color to go to
+        name: name of the command
+        speed: driving speed of the robot in mm/s
+        gain: gain of the line following controller in degrees per % of deviation from the threshold
+        """
+        self.color = color
+        self.name = name
+        self.speed = speed
+        self.gain = gain
+    
+    def run(self, robot):
+        # Calculate the light threshold. Choose values based on your measurements.
+        threshold = (self.inside + self.outside) / 2
+
+        # Start following the line until the end_fn returns True.
+        while self.end_fn() == False:
+
+            # Calculate the deviation from the threshold.
+            deviation = robot.light_sensor.reflection() - threshold
+
+            # Calculate the turn rate.
+            turn_rate = self.gain * deviation
+
+            # Set status
+            #self.status = f"deviation: {deviation}, turn_rate: {turn_rate}"
+
+            # Set the drive base speed and turn rate.
+            robot.drivebase.drive(self.speed, turn_rate)
